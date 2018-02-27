11091600ba54e94bd8574143fd5ec49a39b59245
Roman Golyshev <anstreth@gmail.com>
2018-02-28 01:50:15 +0300

    remove everything (

--- src/main/kotlin/ru/spbau/cliapp/Application.kt
+++ src/main/kotlin/ru/spbau/cliapp/Application.kt
@@ -1,29 +1,5 @@
 package ru.spbau.cliapp
 
-import ru.spbau.cliapp.task.TaskBuilder
-import java.io.PipedInputStream
-import java.io.PipedOutputStream
-
 fun main(args: Array<String>) {
-    val echo = TaskBuilder {
-        stdout.write(_args.joinToString(" ").toByteArray())
-    }
 
-    val cat = TaskBuilder {
-        if (_args.isEmpty()) {
-            val bytes = ByteArray(1024)
-
-            while (true) {
-                val read = stdin.read(bytes)
-                if (read == -1) break
-                stdout.write(bytes, 0, read)
-            }
-        }
-    }
-
-    val pipeIn = PipedInputStream()
-    val pipeOut = PipedOutputStream(pipeIn)
-
-    echo(System.`in`, pipeOut, arrayOf("hello", "world"))
-    cat(pipeIn, System.out, emptyArray())
 }
--- src/main/kotlin/ru/spbau/cliapp/task/TaskContext.kt
+++
@@ -1,6 +0,0 @@
-package ru.spbau.cliapp.task
-
-import java.io.InputStream
-import java.io.OutputStream
-
-class TaskContext(val stdin: InputStream, val stdout: OutputStream, val _args: Array<String>)
