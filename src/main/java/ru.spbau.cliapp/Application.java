42192a27f1586875ba3116089d5dd52975b8244b
Roman Golyshev <anstreth@gmail.com>
2018-03-01 12:56:06 +0300

    add usage of cat and echo together

--- src/main/java/ru.spbau.cliapp/Application.java
+++ src/main/java/ru.spbau.cliapp/Application.java
@@ -1,10 +1,20 @@
 package ru.spbau.cliapp;
 
+import ru.spbau.cliapp.core.Pipe;
+import ru.spbau.cliapp.core.SystemPipe;
+import ru.spbau.cliapp.task.Cat;
 import ru.spbau.cliapp.task.Echo;
 
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        Echo echo = new Echo();
-        echo.main(System.in, System.out, new String[]{"hello", "world"});
+    public static void main(String[] args) throws IOException {
+        try (Pipe pipe = new SystemPipe()) {
+            Echo echo = new Echo();
+            Cat cat = new Cat();
+            echo.main(System.in, pipe.getOutputStream(), new String[]{"hello", "world"});
+            pipe.getOutputStream().close();
+            cat.main(pipe.getInputStream(), System.out, new String[]{});
+        }
     }
 }
