16f8c41c9ba8f18cf80931df72fcc6793ed88630
Roman Golyshev <anstreth@gmail.com>
2018-03-02 01:42:49 +0300

    refactor Cat

--- src/main/java/ru.spbau.cliapp/task/Cat.java
+++ src/main/java/ru.spbau.cliapp/task/Cat.java
@@ -20,25 +20,42 @@ public class Cat implements Task {
     @Override
     public int main(ProcessContext context, List<String> args) {
         if (args.isEmpty()) {
-            int size;
-            byte[] bytes = new byte[1024];
-            try {
-                while ((size = context.getStdin().read(bytes)) != -1) {
-                    context.getStdout().write(bytes, 0, size);
-                }
-            } catch (IOException e) {
-                return 1;
-            }
+            return printFromStdout(context);
         } else {
-            for (String fileName : args) {
+            return printFiles(context, args);
+        }
+
+    }
+
+    private int printFiles(ProcessContext context, List<String> args) {
+        int errorCode = 0;
+        for (String fileName : args) {
+            try {
+                Path path = context.getWorkingDir().resolve(fileName);
+                byte[] bytes = Files.readAllBytes(path);
+                context.getStdout().write(bytes);
+                context.getStdout().write("\n".getBytes());
+            } catch (IOException e) {
+                errorCode = 1;
                 try {
-                    Path path = context.getWorkingDir().resolve(fileName);
-                    byte[] bytes = Files.readAllBytes(path);
-                    context.getStdout().write(bytes);
-                    context.getStdout().write("\n".getBytes());
+                    context.getErr().write(("Error priniting file " + fileName).getBytes());
                 } catch (IOException ignored) {
                 }
             }
+        }
+
+        return errorCode;
+    }
+
+    private int printFromStdout(ProcessContext context) {
+        int size;
+        byte[] bytes = new byte[1024];
+        try {
+            while ((size = context.getStdin().read(bytes)) != -1) {
+                context.getStdout().write(bytes, 0, size);
+            }
+        } catch (IOException e) {
+            return 1;
         }
 
         return 0;
