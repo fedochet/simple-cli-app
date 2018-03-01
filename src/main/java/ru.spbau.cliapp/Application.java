f3ff1782adec20c7cdda032b69f76703dbe09e70
Roman Golyshev <anstreth@gmail.com>
2018-03-01 12:44:36 +0300

    add Echo task

--- src/main/java/ru.spbau.cliapp/Application.java
+++ src/main/java/ru.spbau.cliapp/Application.java
@@ -1,9 +1,10 @@
 package ru.spbau.cliapp;
 
-import java.io.IOException;
+import ru.spbau.cliapp.task.Echo;
 
 public class Application {
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
+        Echo echo = new Echo();
+        echo.main(System.in, System.out, new String[]{"hello", "world"});
     }
 }
---
+++ src/main/java/ru.spbau.cliapp/task/Echo.java
@@ -0,0 +1,20 @@
+package ru.spbau.cliapp.task;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Echo {
+    public int main(InputStream stdin, OutputStream stdout, String[] args) {
+        try {
+            for (String arg : args) {
+                stdout.write(arg.getBytes());
+                stdout.write(" ".getBytes());
+            }
+        } catch (IOException e) {
+            return 1;
+        }
+
+        return 0;
+    }
+}
