fb10cf07893c89cdaf52483eb6a220c548f03347
Roman Golyshev <anstreth@gmail.com>
2018-03-01 14:10:30 +0300

    add usage of Workflow

--- src/main/java/ru.spbau.cliapp/Application.java
+++ src/main/java/ru.spbau.cliapp/Application.java
@@ -1,21 +1,33 @@
 package ru.spbau.cliapp;
 
-import ru.spbau.cliapp.core.Pipe;
-import ru.spbau.cliapp.core.SystemPipe;
-import ru.spbau.cliapp.task.ShellProcess;
+import ru.spbau.cliapp.core.TaskInfo;
+import ru.spbau.cliapp.core.Workflow;
 import ru.spbau.cliapp.task.Cat;
 import ru.spbau.cliapp.task.Echo;
+import ru.spbau.cliapp.task.Task;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) throws IOException {
-        Pipe pipe = new SystemPipe();
-        Echo echo = new Echo();
         Cat cat = new Cat();
-        ShellProcess.createProcess(echo).execute(System.in, pipe.getOutputStream(), Arrays.asList("hello", "world"));
-        ShellProcess.createProcess(cat).execute(pipe.getInputStream(), System.out, Collections.emptyList());
+        Echo echo = new Echo();
+        Map<String, Task> namedTasks = new HashMap<>();
+        namedTasks.put("cat", cat);
+        namedTasks.put("echo", echo);
+
+        Workflow workflow = new Workflow(
+            namedTasks, Arrays.asList(
+                new TaskInfo("echo", Arrays.asList("hello")),
+                new TaskInfo("cat", Collections.emptyList()),
+                new TaskInfo("cat", Collections.emptyList())
+            )
+        );
+
+        workflow.execute(System.in, System.out);
     }
 }
