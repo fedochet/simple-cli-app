5c1495875a94de25747c5878844b1e914dc22139
Roman Golyshev <anstreth@gmail.com>
2018-03-01 22:47:24 +0300

    add more test to the wc, fix it on the way

--- src/main/java/ru.spbau.cliapp/task/Wc.java
+++ src/main/java/ru.spbau.cliapp/task/Wc.java
@@ -5,14 +5,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.MessageFormat;
+import java.util.Arrays;
 import java.util.List;
 
 public class Wc implements Task {
     @Override
     public int main(InputStream stdin, OutputStream stdout, List<String> args) {
-        int newlines = 0;
+        long newlines = 0;
         int bytesCount = 0;
-        int words = 0;
+        long words = 0;
 
         ByteArrayOutputStream bytesBuffer = new ByteArrayOutputStream();
         if (args.isEmpty()) {
@@ -25,8 +26,8 @@ public class Wc implements Task {
 
                 bytesCount = bytesBuffer.size();
                 String result = bytesBuffer.toString();
-                words = result.trim().split("\\s+").length - 1;
-                newlines = result.split("[\n|\r]").length - 1;
+                words = countWords(result);
+                newlines = countNewlines(result);
 
                 stdout.write(MessageFormat.format("{0} {1} {2}\n", newlines, words, bytesCount).getBytes());
             } catch (IOException e) {
@@ -35,5 +36,16 @@ public class Wc implements Task {
         }
 
         return 0;
+    }
+
+    private long countNewlines(String result) {
+        return result.chars().filter(c -> (char)c == '\n').count();
+    }
+
+    private long countWords(String result) {
+        return Arrays.stream(result.split("\n"))
+            .filter(s -> !s.isEmpty())
+            .mapToLong(s -> s.split("\\s+").length)
+            .sum();
     }
 }
--- src/test/java/ru/spbau/cliapp/task/WcTest.java
+++ src/test/java/ru/spbau/cliapp/task/WcTest.java
@@ -1,9 +1,11 @@
 package ru.spbau.cliapp.task;
 
 import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static java.util.Collections.emptyList;
 import static org.junit.jupiter.api.Assertions.*;
@@ -13,12 +15,36 @@ class WcTest {
 
     @Test
     void empty_string_gives_correct_answer() {
-        ByteArrayInputStream emptyStringInput = new ByteArrayInputStream("".getBytes());
+        InputStream emptyStringInput = getInput("");
         ByteOutputStream output = new ByteOutputStream();
 
         wc.main(emptyStringInput, output, emptyList());
 
-        String result = new String(output.getBytes(), 0, output.getCount());
-        assertEquals("0 0 0\n", result);
+        assertEquals("0 0 0\n", output.toString());
+    }
+
+    @Test
+    void hello_world_string_gives_correct_result() {
+        InputStream emptyStringInput = getInput("hello world");
+        ByteOutputStream output = new ByteOutputStream();
+
+        wc.main(emptyStringInput, output, emptyList());
+
+        assertEquals("0 2 11\n", output.toString());
+    }
+
+    @Test
+    void newlines_are_correctly_counted() {
+        InputStream emptyStringInput = getInput("hello\nworld\n");
+        ByteOutputStream output = new ByteOutputStream();
+
+        wc.main(emptyStringInput, output, emptyList());
+
+        assertEquals("2 2 12\n", output.toString());
+    }
+
+    @NotNull
+    private InputStream getInput(String s) {
+        return new ByteArrayInputStream(s.getBytes());
     }
 }
