c2f5e7633978290577a8c0ae4e0650d48d22c7a7
Roman Golyshev <anstreth@gmail.com>
2018-03-01 13:04:09 +0300

    extract AbstractTask class

---
+++ src/main/java/ru.spbau.cliapp/task/AbstractTask.java
@@ -0,0 +1,21 @@
+package ru.spbau.cliapp.task;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public abstract class AbstractTask implements Task {
+    @Override
+    public int execute(InputStream stdin, OutputStream stdout, String[] args) {
+        int exitCode = main(stdin, stdout, args);
+        try {
+            stdout.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot close output stream", e);
+        }
+        return exitCode;
+
+    }
+
+    protected abstract int main(InputStream stdin, OutputStream stdout, String[] args);
+}
--- src/main/java/ru.spbau.cliapp/task/Echo.java
+++ src/main/java/ru.spbau.cliapp/task/Echo.java
@@ -4,20 +4,10 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class Echo implements Task {
+public class Echo extends AbstractTask {
+
     @Override
-    public int execute(InputStream stdin, OutputStream stdout, String[] args) {
-        int exitCode = main(stdin, stdout, args);
-        try {
-            stdout.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot close output stream", e);
-        }
-        return exitCode;
-
-    }
-
-    private int main(InputStream stdin, OutputStream stdout, String[] args) {
+    protected int main(InputStream stdin, OutputStream stdout, String[] args) {
         try {
             for (String arg : args) {
                 stdout.write(arg.getBytes());
