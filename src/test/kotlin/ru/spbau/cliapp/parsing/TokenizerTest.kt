46fd92e6015f6a0f2f10a49b317039ee3e5db273
Roman Golyshev <anstreth@gmail.com>
2018-03-03 14:21:59 +0300

    add AssertJ dependency, use in in `TokenizerTest`

--- src/test/kotlin/ru/spbau/cliapp/parsing/TokenizerTest.kt
+++ src/test/kotlin/ru/spbau/cliapp/parsing/TokenizerTest.kt
@@ -1,7 +1,6 @@
 package ru.spbau.cliapp.parsing
 
-import junit.framework.TestCase.assertEquals
-import junit.framework.TestCase.assertTrue
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class TokenizerTest {
@@ -9,45 +8,46 @@ class TokenizerTest {
 
     @Test
     fun `empty string tokenized to empty list`() {
-        assertTrue(tokenizer.tokenize("").isEmpty())
+        assertThat(tokenizer.tokenize("")).isEmpty()
     }
 
     @Test
     fun `one command is tokenized into string token`() {
-        assertEquals(listOf(StringToken("echo")), tokenizer.tokenize("echo"))
+        assertThat(tokenizer.tokenize("echo")).containsExactly(StringToken("echo"))
     }
 
     @Test
     fun `many commands are tokenised into correct tokens`() {
-        assertEquals(
-                listOf(StringToken("echo"), StringToken("hello"), StringToken("one"), StringToken("two")),
-                tokenizer.tokenize("echo hello one two")
-        )
+        assertThat(tokenizer.tokenize("echo hello one two"))
+                .containsExactly(StringToken("echo"), StringToken("hello"), StringToken("one"), StringToken("two"))
+
     }
 
     @Test
     fun `many spaces between words ignored`() {
-        assertEquals(
-                listOf(StringToken("echo"), StringToken("hello")),
-                tokenizer.tokenize("     echo     hello    ")
-        )
+        assertThat(tokenizer.tokenize("     echo     hello    "))
+                .containsExactly(StringToken("echo"), StringToken("hello"))
     }
 
     @Test
     fun `double quotes string is found`() {
-        assertEquals(listOf(DoubleQuotesToken("echo")), tokenizer.tokenize("\"echo\""))
+        assertThat(tokenizer.tokenize("\"echo\"")).containsExactly(DoubleQuotesToken("echo"))
     }
 
     @Test
     fun `pipe symbol is recognized`() {
-        assertEquals(listOf(VerticalBar), tokenizer.tokenize("|"))
+        assertThat(tokenizer.tokenize("|")).containsExactly(VerticalBar)
     }
 
     @Test
     fun `pipe is recognised with other things`() {
-        assertEquals(
-                listOf(StringToken("echo"), DoubleQuotesToken("hello world"), VerticalBar, StringToken("cat"), SingleQuotesToken("-v")),
-                tokenizer.tokenize("echo \"hello world\" | cat '-v'")
-        )
+        assertThat(tokenizer.tokenize("echo \"hello world\" | cat '-v'"))
+                .containsExactly(
+                        StringToken("echo"),
+                        DoubleQuotesToken("hello world"),
+                        VerticalBar,
+                        StringToken("cat"),
+                        SingleQuotesToken("-v")
+                )
     }
 }
