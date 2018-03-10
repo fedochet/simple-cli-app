3beeca82c5cb01c4b5e6344efc8bdc17e8f8ff17
Roman Golyshev <anstreth@gmail.com>
2018-03-10 22:04:44 +0300

    fix echo

--- src/main/java/ru.spbau.cliapp/task/Echo.java
+++ src/main/java/ru.spbau.cliapp/task/Echo.java
@@ -4,16 +4,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Echo implements Task {
 
     @Override
     public int main(InputStream stdin, OutputStream stdout, List<String> args) {
         try {
-            for (String arg : args) {
-                stdout.write(arg.getBytes());
-                stdout.write(" ".getBytes());
-            }
+            stdout.write(args.stream().collect(Collectors.joining(" ")).concat("\n").getBytes());
             stdout.write("\n".getBytes());
         } catch (IOException e) {
             return 1;
